26235a0ac454d7074e16fcb48782a04ab986d323
gahyeon <gus_05@naver.com>
2025-01-09 20:28:25 +0900

    init: 스토리북 vanilla extract 플러그인 세팅

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,30 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+
+  async viteFinal(config) {
+    config.plugins = config.plugins || [];
+    config.plugins.push(
+      vanillaExtractPlugin({
+        identifiers: ({ hash }) => `_${hash}`,
+      }),
+    );
+    return config;
   },
 };
 export default config;
