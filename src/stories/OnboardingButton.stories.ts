1c550c20ca63bafd78128930c0934a9856c9a4b9
Taew00k <davis0625@dgu.ac.kr>
2025-01-14 00:20:28 +0900

    rename: 폴더명 소문자로 변경

--- src/stories/OnboardingButton.stories.ts
+++ src/stories/OnboardingButton.stories.ts
@@ -1,5 +1,5 @@
 import Icon from '@assets/svgs';
-import onboardingBtn from '@components/common/onboarding/onboardingBtn';
+import onboardingBtn from '@components/common/button/onboarding/onboardingBtn';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
