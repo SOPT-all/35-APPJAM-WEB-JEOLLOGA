456f604b7f7c63d9f2915d4bbe97bfb633e6f257
Taew00k <davis0625@dgu.ac.kr>
2025-01-14 00:10:46 +0900

    feat: 온보딩 버튼 구현

---
+++ src/stories/OnboardingButton.stories.ts
@@ -0,0 +1,60 @@
+import Icon from '@assets/svgs';
+import onboardingBtn from '@components/common/onboarding/onboardingBtn';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/OnboardingButton',
+  component: onboardingBtn,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    btnText: {
+      control: { type: 'text' },
+      description: 'Text displayed on the button',
+    },
+    isActive: {
+      control: { type: 'boolean' },
+      description: 'Determines if the button is active',
+    },
+    leftIcon: {
+      control: { type: 'select' },
+      options: Object.keys(Icon),
+      description: 'Icon displayed on the left side of the button',
+    },
+  },
+  args: {
+    btnText: '이슬람교',
+    isActive: false,
+    leftIcon: undefined, // No icon by default
+  },
+} satisfies Meta<typeof onboardingBtn>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Active: Story = {
+  args: {
+    btnText: '60대 이상',
+    isActive: true,
+  },
+};
+
+export const WithIcon: Story = {
+  args: {
+    btnText: '있음',
+    leftIcon: 'IcnO',
+  },
+};
+
+export const WithIconActive: Story = {
+  args: {
+    btnText: '없음',
+    leftIcon: 'IcnX',
+    isActive: true,
+  },
+};
