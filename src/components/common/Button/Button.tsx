fedc61a2b8b19dbaeb259985a29bec61d5f8249f
moonseonghui <msh9980@naver.com>
2025-01-13 17:47:21 +0900

    feat: Button 컴포넌트 구현

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,26 +1,36 @@
+import Icon from '@assets/svgs';
+
 import buttonStyle from './button.css';
 
 interface ButtonProps {
-  variant?: 'primary' | 'secondary' | 'tertiary' | 'outline';
-  size?: 'xLarge' | 'large' | 'medium';
-  isDisabled?: boolean;
+  variant?: 'primary' | 'grayOutlined' | 'blackOutlined';
+  size?: 'large' | 'medium' | 'small';
   label: string;
+  leftIcon?: keyof typeof Icon;
+  rightIcon?: keyof typeof Icon;
+  onClick?: () => void;
+  isActive?: boolean;
 }
 
 const Button = ({
   variant = 'primary',
   size = 'medium',
-  isDisabled = false,
   label,
-  ...props
+  leftIcon,
+  rightIcon,
+  onClick,
+  isActive = false,
 }: ButtonProps) => {
+  const SelectedLeftIcon = leftIcon ? Icon[leftIcon] : null;
+  const SelectedRightIcon = rightIcon ? Icon[rightIcon] : null;
+
   return (
     <button
-      type="button"
-      className={buttonStyle({ color: variant, size: size })}
-      disabled={isDisabled}
-      {...props}>
+      className={buttonStyle({ color: variant, size, active: isActive ? true : false })}
+      onClick={onClick}>
+      {SelectedLeftIcon && <SelectedLeftIcon />}
       {label}
+      {SelectedRightIcon && <SelectedRightIcon />}
     </button>
   );
 };
