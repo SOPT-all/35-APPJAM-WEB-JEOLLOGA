c62d70e54144a379ad14936d4d3afe64a1e77f8f
moonseonghui <msh9980@naver.com>
2025-01-13 23:28:30 +0900

    feat: input value 지우는 로직 추가

--- src/components/common/Button/searchBar/SearchBar.tsx
+++ src/components/common/Button/searchBar/SearchBar.tsx
@@ -1,15 +1,31 @@
 import Icon from '@assets/svgs';
+import { useState } from 'react';
 
 import * as styles from './searchBar.css';
 
 const SearchBar = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleClearInput = () => {
+    setInputValue('');
+  };
+
   return (
     <div className={styles.searchBarContainer}>
       <div className={styles.searchBarLayout}>
         <Icon.IcnSearchMediumGray />
-        <input className={styles.inputStyle} placeholder="텍스트 텍스트" />
+        <input
+          className={styles.inputStyle}
+          placeholder="텍스트 텍스트"
+          value={inputValue}
+          onChange={handleChangeInput}
+        />
       </div>
-      <Icon.IcnCloseLargeGray />
+      <Icon.IcnCloseLargeGray onClick={handleClearInput} />
     </div>
   );
 };
