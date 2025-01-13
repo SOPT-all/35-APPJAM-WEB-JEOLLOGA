b319d0911ec3aa0a189a1cd8eccd9f5cfbc4556d
Taew00k <davis0625@dgu.ac.kr>
2025-01-13 16:44:04 +0900

    remove: app 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import PageBtn from '@components/common/Button/pageBtn/pageBtn';
-
 const App = () => {
-  return (
-    <div>
-      <PageBtn pageNum={1} currentPageNum={2} />
-      <PageBtn pageNum={2} currentPageNum={2} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
