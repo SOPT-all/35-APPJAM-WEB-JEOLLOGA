9552e81f69fbc0d1dfee6fb82111a4181dd11ab6
gahyeon <gus_05@naver.com>
2025-01-13 14:24:26 +0900

    feat: 아이콘 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,1 +1,23 @@
-// index.ts
+export { default as Logo } from './logo.svg';
+export { default as IcnArrowBlackLeft } from './icn_arrow_black_left.svg';
+export { default as IcnArrowGrayDown } from './icn_arrow_gray_down.svg';
+export { default as IcnArrowGrayRight } from './icn_arrow_gray_right.svg';
+export { default as IcnArrowGrayUp } from './icn_arrow_gray_up.svg';
+export { default as IcnCloseLargeGray } from './icn_close_large_gray.svg';
+export { default as IcnCloseSmallGray } from './icn_close_small_gray.svg';
+export { default as IcnFilter } from './icn_filter.svg';
+export { default as IcnHome } from './icn_home.svg';
+export { default as IcnInsta } from './icn_insta.svg';
+export { default as IcnLargeHeartPink } from './icn_large_heart_pink.svg';
+export { default as IcnLineArrowLargeRight } from './icn_linearrow_large_right.svg';
+export { default as IcnLineArrowSmallLeft } from './icn_linearrow_small_left.svg';
+export { default as IcnLineArrowSmallRight } from './icn_linearrow_small_right.svg';
+export { default as IcnMyPage } from './icn_mypage.svg';
+export { default as IcnO } from './icn_o.svg';
+export { default as IcnPaste } from './icn_paste.svg';
+export { default as IcnReset } from './icn_reset.svg';
+export { default as IcnSearchLargeBlack } from './icn_search_large_black.svg';
+export { default as IcnSearchMediumGray } from './icn_search_medium_gray.svg';
+export { default as IcnSmallHeart } from './icn_small_heart.svg';
+export { default as IcnWish } from './icn_wish.svg';
+export { default as IcnX } from './icn_x.svg';
