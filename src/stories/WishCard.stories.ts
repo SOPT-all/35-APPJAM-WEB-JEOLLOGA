6b7b34211376c8dfca82ed44766223f414b38a7a
moonseonghui <msh9980@naver.com>
2025-01-15 02:26:09 +0900

    feat: WishCard storybook에 horizontal, vertical 경우 추가

--- src/stories/WishCard.stories.ts
+++ src/stories/WishCard.stories.ts
@@ -34,6 +34,10 @@ const meta = {
     liked: {
       control: { type: 'boolean' },
     },
+    layout: {
+      control: { type: 'select' },
+      options: ['vertical', 'horizontal'],
+    },
   },
   args: {
     id: 1,
@@ -45,6 +49,7 @@ const meta = {
     imgUrl:
       'https://file.percenty.co.kr/public/65a89c361aa1f25215b17f4a/products/660db89df900ac2f15094bc4/47272ce2-f477-4472-955e-f2e8eddc521e.jpg',
     liked: false,
+    layout: 'horizontal',
   },
 } satisfies Meta<typeof WishCard>;
 
@@ -53,3 +58,15 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const horizontalWishCard: Story = {
+  args: {
+    layout: 'horizontal',
+  },
+};
+
+export const verticalWishCard: Story = {
+  args: {
+    layout: 'vertical',
+  },
+};
