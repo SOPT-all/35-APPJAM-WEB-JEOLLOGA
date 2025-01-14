3aaa4a0311e74d72fce9042ba6d472658761c046
moonseonghui <msh9980@naver.com>
2025-01-14 23:06:01 +0900

    fix: wishCard storybook argTypes 추가

--- src/stories/WishCard.stories.ts
+++ src/stories/WishCard.stories.ts
@@ -1,18 +1,6 @@
 import WishCard from '@components/card/wishCard/WishCard';
 import type { Meta, StoryObj } from '@storybook/react';
 
-const dummy = {
-  id: 1,
-  templeName: '봉인사',
-  templestayName: '사불산(四佛山)... 옛길을 걷다',
-  tag: '연예인이 다녀간',
-  region: '서울',
-  type: '휴식형',
-  imgUrl:
-    'https://file.percenty.co.kr/public/65a89c361aa1f25215b17f4a/products/660db89df900ac2f15094bc4/47272ce2-f477-4472-955e-f2e8eddc521e.jpg',
-  liked: true,
-};
-
 const meta = {
   title: 'Common/card/WishCard',
   component: WishCard,
@@ -20,14 +8,48 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  argTypes: {
+    id: {
+      control: { type: 'number' },
+    },
+    templeName: {
+      control: { type: 'text' },
+    },
+
+    templestayName: {
+      control: { type: 'text' },
+    },
+    tag: {
+      control: { type: 'text' },
+    },
+    region: {
+      control: { type: 'text' },
+    },
+    type: {
+      control: { type: 'text' },
+    },
+    imgUrl: {
+      control: { type: 'text' },
+    },
+    liked: {
+      control: { type: 'boolean' },
+    },
+  },
+  args: {
+    id: 1,
+    templeName: '봉인사',
+    templestayName: '사불산(四佛山)... 옛길을 걷다',
+    tag: '연예인이 다녀간',
+    region: '서울',
+    type: '휴식형',
+    imgUrl:
+      'https://file.percenty.co.kr/public/65a89c361aa1f25215b17f4a/products/660db89df900ac2f15094bc4/47272ce2-f477-4472-955e-f2e8eddc521e.jpg',
+    liked: false,
+  },
 } satisfies Meta<typeof WishCard>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    ...dummy,
-  },
-};
+export const Default: Story = {};
