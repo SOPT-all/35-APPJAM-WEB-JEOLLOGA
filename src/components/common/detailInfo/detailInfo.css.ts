219f3590c89f435a2b6ee6de3a89dee4a2d49ff7
gahyeon <gus_05@naver.com>
2025-01-14 15:29:45 +0900

    feat: DetailInfo 컴포넌트 구현

---
+++ src/components/common/detailInfo/detailInfo.css.ts
@@ -0,0 +1,20 @@
+import theme from '@styles/theme.css';
+import { style } from '@vanilla-extract/css';
+
+export const infoBox = style({
+  width: '33.5rem',
+  padding: '1.2rem 1.4rem',
+  display: 'flex',
+  gap: '0.8rem',
+  borderRadius: '4px',
+  backgroundColor: theme.COLORS.gray1,
+});
+
+export const title = style({
+  ...theme.FONTS.c3Sb14,
+});
+
+export const content = style({
+  ...theme.FONTS.c2R14,
+  color: theme.COLORS.gray10,
+});
