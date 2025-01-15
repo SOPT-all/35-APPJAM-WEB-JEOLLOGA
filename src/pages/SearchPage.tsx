d30d69f58b2a55137ec328324bc1d836d6ac038d
moonseonghui <msh9980@naver.com>
2025-01-16 05:48:09 +0900

    feat: searchPage 구현

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,5 +1,28 @@
+import DetailTitle from '@components/detailTitle/DetailTitle';
+import RecentBtnBox from '@components/search/recentBtn/RecentBtnBox';
+import SearchHeader from '@components/search/searchHeader/SearchHeader';
+
+const recentData = [
+  { id: 1, content: '불ㅇ사' },
+  { id: 2, content: '봉인사' },
+  { id: 3, content: '숙구사' },
+  { id: 4, content: '불ㅇ사' },
+  { id: 5, content: '봉인사' },
+  { id: 6, content: '숙구사' },
+  { id: 7, content: '불ㅇ사' },
+  { id: 8, content: '불ㅇ사' },
+  { id: 9, content: '봉인사' },
+  { id: 10, content: '숙구사' },
+];
+
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <>
+      <SearchHeader />
+      <DetailTitle title="최근 검색" isTotal size="small" rightBtnLable="전체 삭제" />
+      <RecentBtnBox data={recentData} />
+    </>
+  );
 };
 
 export default SearchPage;
