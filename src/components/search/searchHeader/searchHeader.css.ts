35ee4ab5dfb95b884b82b5127e7f09a284f2c206
moonseonghui <msh9980@naver.com>
2025-01-16 05:11:43 +0900

    rename: search 관련 로직 search 폴더로 이동

---
+++ src/components/search/searchHeader/searchHeader.css.ts
@@ -0,0 +1,13 @@
+import { style } from '@vanilla-extract/css';
+
+const searchHeaderStyle = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.8rem',
+  width: '34.2rem',
+  height: '4.4rem',
+
+  margin: '0 0.8rem 2rem',
+});
+
+export default searchHeaderStyle;
