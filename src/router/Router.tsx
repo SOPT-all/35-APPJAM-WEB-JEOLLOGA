4d046514af60d5484327144dff72842a38c58674
moonseonghui <msh9980@naver.com>
2025-01-15 23:03:09 +0900

    feat: router 추가

---
+++ src/router/Router.tsx
@@ -0,0 +1,39 @@
+import { lazy, Suspense } from 'react';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import App from 'src/App';
+
+const HomePage = lazy(() => import('@pages/HomePage'));
+const SearchPage = lazy(() => import('@pages/SearchPage'));
+const ErrorPage = lazy(() => import('@pages/ErrorPage'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'search',
+        element: <SearchPage />,
+      },
+    ],
+  },
+
+  {
+    path: '*',
+    element: <Navigate to="/" replace />,
+  },
+]);
+
+// TODO: 로딩 화면 추가
+const Router = () => (
+  <Suspense fallback={<div>로딩 화면 추가</div>}>
+    <RouterProvider router={router} />
+  </Suspense>
+);
+
+export default Router;
