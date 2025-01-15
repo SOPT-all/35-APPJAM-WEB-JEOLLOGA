5dbc8aa1834c95f4a1b600dbf253548f4ed76514
moonseonghui <msh9980@naver.com>
2025-01-15 15:20:57 +0900

    test: ButtonBar storybook 추가

---
+++ src/stories/ButtonBar.stories.ts
@@ -0,0 +1,26 @@
+import ButtonBar from '@components/common/button/buttonBar/ButtonBar';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Common/Button/ButtonBar',
+  component: ButtonBar,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      control: { type: 'select' },
+      options: ['reset', 'wish'],
+    },
+  },
+  args: {
+    type: 'wish',
+  },
+} satisfies Meta<typeof ButtonBar>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
