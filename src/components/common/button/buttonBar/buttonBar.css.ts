8c44a4b4c35c98b3f24b997f79275c257becc8fd
moonseonghui <msh9980@naver.com>
2025-01-15 15:20:34 +0900

    feat: ButtonBar 컴포넌트 구현

---
+++ src/components/common/button/buttonBar/buttonBar.css.ts
@@ -0,0 +1,15 @@
+import { style } from '@vanilla-extract/css';
+
+const buttonBarContainer = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '37.5rem',
+  height: '7.2rem',
+  padding: '1rem 2rem',
+  boxSizing: 'border-box',
+
+  boxShadow: `0px -4px 16px 0px rgba(0, 0, 0, 0.05)`,
+});
+
+export default buttonBarContainer;
