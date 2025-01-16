294c0d8f65c92c9c1de83f8197bee8295e2154ca
gahyeon <gus_05@naver.com>
2025-01-17 00:54:25 +0900

    feat: 템플스테이 둘러보기 버튼 구현

---
+++ src/components/card/lookCard/LookCard.tsx
@@ -0,0 +1,33 @@
+import BasicBtn from '@components/common/button/basicBtn/BasicBtn';
+
+import * as styles from './lookCard.css';
+
+interface LookCardProps {
+  name: string;
+}
+
+const LookCard = ({ name }: LookCardProps) => {
+  return (
+    <section className={styles.cardWrapper}>
+      <div className={styles.textBox}>
+        <span>
+          <span className={styles.name}>{name}</span> 님은
+          <br />
+          어떤 템플스테이를
+          <br />
+          원하시나요?
+        </span>
+        <div>
+          <BasicBtn
+            label="둘러보기"
+            size="large"
+            isActive={true}
+            rightIcon="IcnLineArrowLargeRight"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LookCard;
---
+++ src/components/card/lookCard/lookCard.css.ts
@@ -0,0 +1,23 @@
+import theme from '@styles/theme.css';
+import { style } from '@vanilla-extract/css';
+
+export const cardWrapper = style({
+  backgroundImage: 'url(src/assets/images/home_card_look.png)',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  width: '33.5rem',
+  height: '33.1rem',
+  borderRadius: 8,
+  padding: '2rem',
+});
+
+export const textBox = style({
+  ...theme.FONTS.b0R22,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.2rem',
+});
+
+export const name = style({
+  ...theme.FONTS.h0Sb22,
+});
