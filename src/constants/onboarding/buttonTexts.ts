335549ed295bb88d7692857d6b0f2b7c138fbe04
bykbyk0401 <bykbyk0401@gmail.com>
2025-01-17 16:51:23 +0900

    feat: 온보딩 상수 설정

---
+++ src/constants/onboarding/buttonTexts.ts
@@ -0,0 +1,8 @@
+const BUTTON_TEXTS = {
+  age: ['10대', '20대', '30대', '40대', '50대', '60대'],
+  gender: ['남자', '여자'],
+  religion: ['불교', '기독교', '천주교', '힌두교', '이슬람교', '무교', '기타'],
+  experience: ['있음', '없음'],
+};
+
+export default BUTTON_TEXTS;
---
+++ src/constants/onboarding/onboardingSteps.ts
@@ -0,0 +1,8 @@
+export const ONBOARDING_STEPS = [
+  { id: '나이', title: '나이를 알려주세요' },
+  { id: '성별', title: '성별을 알려주세요' },
+  { id: '종교', title: '종교가 있으신가요?' },
+  { id: '이용경험', title: '템플스테이 이용 경험이 있으신가요?' },
+];
+
+export const COMMON_DESCRIPTION = '서비스 발전 이외의 목적으로는 사용하지 않아요!';
