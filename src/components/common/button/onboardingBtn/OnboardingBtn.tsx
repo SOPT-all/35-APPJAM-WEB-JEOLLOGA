98d6b0524933af918a2400442cd1894a9df980ab
bykbyk0401 <bykbyk0401@gmail.com>
2025-01-18 02:19:38 +0900

    feat: 온보딩 버튼 클릭이벤트 추가

--- src/components/common/button/onboardingBtn/OnboardingBtn.tsx
+++ src/components/common/button/onboardingBtn/OnboardingBtn.tsx
@@ -6,13 +6,14 @@ interface OnboardingBtnProps {
   btnText: string;
   isActive?: boolean;
   leftIcon?: keyof typeof Icon;
+  onClick?: () => void;
 }
 
-const OnboardingBtn = ({ btnText, leftIcon, isActive = false }: OnboardingBtnProps) => {
+const OnboardingBtn = ({ btnText, leftIcon, isActive = false, onClick }: OnboardingBtnProps) => {
   const SelectedLeftIcon = leftIcon ? Icon[leftIcon] : null;
 
   return (
-    <button className={OnboardingBtnStyle({ active: isActive ? true : false })}>
+    <button className={OnboardingBtnStyle({ active: isActive ? true : false })} onClick={onClick}>
       <div className={btnContentStyle}>
         {SelectedLeftIcon && <SelectedLeftIcon />}
         <p>{btnText}</p>
