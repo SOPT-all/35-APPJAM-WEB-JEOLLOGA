7360e8c6aca8986f981fe0aa10e6d251c9684aad
Taew00k <davis0625@dgu.ac.kr>
2025-01-17 17:36:05 +0900

    feat: 스토리북 params 설정

--- src/stories/LargeMap.stories.ts
+++ src/stories/LargeMap.stories.ts
@@ -1,11 +1,23 @@
 import LargeMap from '@components/templeDetail/naverMap/largeMap/LargeMap';
 import type { Meta, StoryObj } from '@storybook/react';
+import { reactRouterParameters } from 'storybook-addon-react-router-v6';
 
 const meta = {
   title: 'Common/Temple/TempleDetail/LargeMap',
   component: LargeMap,
   parameters: {
     layout: 'centered',
+    reactRouter: reactRouterParameters({
+      location: {
+        pathParams: {
+          latitude: '37.55433',
+          longitude: '126.9686',
+        },
+      },
+      routing: {
+        path: '/map?latitude=37.55433&longitude=126.9686',
+      },
+    }),
   },
   tags: ['autodocs'],
 } satisfies Meta<typeof LargeMap>;
@@ -14,4 +26,6 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {},
+};
