9e9c7740b1a8dc8c11f040d3124adfe3c2561c3a
gahyeon <gus_05@naver.com>
2025-01-19 14:51:09 +0900

    refactor: 캐러셀 커스텀 훅 적용

--- src/components/carousel/popularCarousel/PopularCarousel.tsx
+++ src/components/carousel/popularCarousel/PopularCarousel.tsx
@@ -1,6 +1,7 @@
 import PopularCard from '@components/card/popularCard/PopularCard';
+import useCarousel from '@hooks/useCarousel';
 import registDragEvent from '@utils/registDragEvent';
-import React, { useState, useRef } from 'react';
+import React from 'react';
 
 import * as styles from './popularCarousel.css';
 
@@ -37,54 +38,33 @@ const stayData = {
 };
 
 const PopularCarousel = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [transX, setTransX] = useState(0);
-  const carouselRef = useRef<HTMLDivElement>(null);
-
-  const moveDistance = 355;
+  const { carouselRef, transformStyle, handleDragChange, handleDragEnd } = useCarousel({
+    itemCount: stayData.rankings.length,
+    moveDistance: 355,
+  });
 
   return (
     <section ref={carouselRef} className={styles.carouselWrapper}>
       <div
         className={styles.carouselContainer}
-        style={{
-          transform: `translateX(${-currentIndex * moveDistance + transX}px)`,
-          transition: `transform 200ms ease-in-out 0s`,
-        }}
+        style={transformStyle}
         {...registDragEvent({
-          onDragChange: (deltaX) => {
-            setTransX(deltaX);
-          },
-          onDragEnd: (deltaX) => {
-            const maxIndex = stayData.rankings.length - 1;
-
-            // 드래그를 왼쪽으로 넘겼을 때
-            if (deltaX < -100) {
-              setCurrentIndex(currentIndex + 1 > maxIndex ? maxIndex : currentIndex + 1);
-            }
-            // 드래그를 오른쪽으로 넘겼을 때
-            if (deltaX > 100) {
-              setCurrentIndex(currentIndex - 1 < 0 ? 0 : currentIndex - 1);
-            }
-
-            setTransX(0);
-          },
+          onDragChange: handleDragChange,
+          onDragEnd: handleDragEnd,
         })}>
-        {stayData.rankings.map((data, id) => (
-          <>
-            <PopularCard
-              key={id}
-              ranking={data.ranking}
-              templeName={data.templeName}
-              templeLoc={data.region}
-              templeImg={data.imgUrl}
-              isLiked={data.liked}
-              tag={data.tag}
-              onClick={() => {
-                alert(`${data.templeName} 클릭됨!`);
-              }}
-            />
-          </>
+        {stayData.rankings.map((data) => (
+          <PopularCard
+            key={data.id}
+            ranking={data.ranking}
+            templeName={data.templeName}
+            templeLoc={data.region}
+            templeImg={data.imgUrl}
+            isLiked={data.liked}
+            tag={data.tag}
+            onClick={() => {
+              alert(`${data.templeName} 클릭됨!`);
+            }}
+          />
         ))}
       </div>
     </section>
