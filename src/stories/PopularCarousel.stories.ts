36bb7db48ae983faf0613ff2bfab5fefc662cf18
gahyeon <gus_05@naver.com>
2025-01-19 17:36:30 +0900

    fix: PopularCarousel 스토리북 경로 수정

--- src/stories/PopularCarousel.stories.ts
+++ src/stories/PopularCarousel.stories.ts
@@ -2,7 +2,7 @@ import PopularCarousel from '@components/carousel/popularCarousel/PopularCarouse
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
-  title: 'Carousel/PopularCarousel',
+  title: 'Carousel/PopularCarousel/PopularCarousel',
   component: PopularCarousel,
   parameters: {
     layout: 'centered',
