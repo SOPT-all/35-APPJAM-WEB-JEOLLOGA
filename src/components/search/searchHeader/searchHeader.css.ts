1c173d85bae16663edfd72d21a1adcc1c3566350
moonseonghui <msh9980@naver.com>
2025-01-20 07:23:38 +0900

    fix: 페이지 스타일 수정

--- src/components/search/searchHeader/searchHeader.css.ts
+++ src/components/search/searchHeader/searchHeader.css.ts
@@ -3,11 +3,12 @@ import { style } from '@vanilla-extract/css';
 const searchHeaderStyle = style({
   display: 'flex',
   alignItems: 'center',
+  justifyContent: 'center',
   gap: '0.8rem',
-  width: '34.2rem',
+  width: '37.5rem',
   height: '4.4rem',
 
-  margin: '0 0.8rem 2rem',
+  marginBottom: '2rem',
 });
 
 export default searchHeaderStyle;
