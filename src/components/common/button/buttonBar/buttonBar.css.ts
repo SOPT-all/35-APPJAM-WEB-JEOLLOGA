b315aeedcc12dd01acb97cbf8ba02fffe6f09e37
moonseonghui <msh9980@naver.com>
2025-01-19 18:58:11 +0900

    feat: storybook 라우터 추가

--- src/components/common/button/buttonBar/buttonBar.css.ts
+++ src/components/common/button/buttonBar/buttonBar.css.ts
@@ -8,7 +8,6 @@ const buttonBarContainer = style({
   height: '7.2rem',
   padding: '1rem 2rem',
   boxSizing: 'border-box',
-  marginLeft: '-2rem',
 
   boxShadow: `0px -4px 16px 0px rgba(0, 0, 0, 0.05)`,
 });
