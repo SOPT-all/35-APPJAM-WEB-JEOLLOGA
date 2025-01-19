81ba136ca4f5f90af8813cd300d9d65511134901
gahyeon <gus_05@naver.com>
2025-01-20 07:59:00 +0900

    chore: 빌드 오류 해결

--- src/components/card/mapCard/Map.tsx
+++ src/components/card/mapCard/Map.tsx
@@ -1,11 +1,10 @@
 import LocBtn from '@components/card/mapCard/LocBtn';
+import mapStyle from '@components/card/mapCard/map.css';
 import REGION_INFOS from '@constants/regionInfos';
-
-import * as styles from './map.css';
 
 const Map = () => {
   return (
-    <div className={styles.mapStyle}>
+    <div className={mapStyle}>
       {Object.entries(REGION_INFOS).map(([region, { top, left }]) => (
         <LocBtn key={region} region={region} top={top} left={left} />
       ))}
