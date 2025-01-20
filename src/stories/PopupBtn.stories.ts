fcd4432d6a84d5443a0dbf024e2feb839ddf8e02
moonseonghui <msh9980@naver.com>
2025-01-20 15:02:20 +0900

    test: popupBtn 컴포넌트 storybook 추가

---
+++ src/stories/PopupBtn.stories.ts
@@ -0,0 +1,29 @@
+import PopupBtn from '@components/common/button/popupBtn/PopupBtn';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Common/Button/PopupBtn',
+  component: PopupBtn,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof PopupBtn>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    color: 'green',
+    label: '로그인하기',
+  },
+};
+
+export const NoHeart: Story = {
+  args: {
+    color: 'gray',
+    label: '취소',
+  },
+};
