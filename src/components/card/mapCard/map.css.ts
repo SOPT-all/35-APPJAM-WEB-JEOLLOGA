094aa7a5418e3ab264fcb93e0a655f62be2c32db
gahyeon <gus_05@naver.com>
2025-01-21 07:07:43 +0900

    fix: 스토리북에서 이미지 안 보이는 오류 수정

--- src/components/card/mapCard/map.css.ts
+++ src/components/card/mapCard/map.css.ts
@@ -1,9 +1,10 @@
 import { style } from '@vanilla-extract/css';
+import mapImage from 'src/assets/images/home_card_map.png';
 
 const mapStyle = style({
   width: '28.9rem',
   height: '40.8rem',
-  backgroundImage: 'url(src/assets/images/home_card_map.png)',
+  backgroundImage: `url(${mapImage})`,
   backgroundSize: 'cover',
   backgroundPosition: 'center',
   position: 'relative',
