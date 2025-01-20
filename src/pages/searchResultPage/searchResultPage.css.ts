11f77ec24d7d3808cedf96d3bfd4bdf711b29763
bykbyk0401 <bykbyk0401@gmail.com>
2025-01-21 02:08:19 +0900

    feat: 배경 색상 적용

--- src/pages/searchResultPage/searchResultPage.css.ts
+++ src/pages/searchResultPage/searchResultPage.css.ts
@@ -7,14 +7,15 @@ export const container = style({
 
 export const headerContainer = style({
   boxShadow: theme.COLORS.filerDropshadow,
-  marginBottom: '2rem',
 });
 
 export const bodyContainer = style({
   display: 'grid',
   gridTemplateRows: '1fr auto',
   justifyItems: 'center',
-  minHeight: 'calc(100vh - 14.2rem)',
+  minHeight: 'calc(100vh - 12.2rem)',
+  paddingTop: '2rem',
   paddingBottom: '4.4rem',
   gap: '3.2rem',
+  backgroundColor: theme.COLORS.gray1,
 });
