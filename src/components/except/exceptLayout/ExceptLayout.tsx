edc270d61e6a1cafbf0764c6e751d5829dc535e3
gahyeon <gus_05@naver.com>
2025-01-20 17:42:42 +0900

    refactor: except info 관련 데이터 constant로 분리

--- src/components/except/exceptLayout/ExceptLayout.tsx
+++ src/components/except/exceptLayout/ExceptLayout.tsx
@@ -1,3 +1,4 @@
+import EXCEPT_INFOS from '@constants/exceptInfos';
 import React from 'react';
 
 import * as styles from './exceptLayout.css';
@@ -7,23 +8,15 @@ interface ExceptLayoutProps {
 }
 
 const ExceptLayout = ({ type }: ExceptLayoutProps) => {
+  const { title, imageSrc, imageAlt, imageWidth, subtitle } = EXCEPT_INFOS[type];
+
   return (
     <section className={styles.exceptWrapper}>
-      <span className={styles.title}>{type === 'loading' ? '' : '잠시 연결이 불안정해요'}</span>
-      <image className={styles.imgContainer}>
-        <img
-          src={
-            type === 'loading'
-              ? 'src/assets/images/img_moktak_hit.png'
-              : 'src/assets/images/img_tree_fall.png'
-          }
-          alt={type === 'loading' ? '로딩 중' : '네트워크 오류'}
-          width={type === 'loading' ? 144 : 171}
-        />
-      </image>
-      <span className={styles.subtitle[type]}>
-        {type === 'loading' ? '연결 중...' : '새로고침 버튼을 눌러주세요!'}
-      </span>
+      <span className={styles.title}>{title}</span>
+      <div className={styles.imgContainer}>
+        <img src={imageSrc} alt={imageAlt} width={imageWidth} />
+      </div>
+      <span className={styles.subtitle[type]}>{subtitle}</span>
     </section>
   );
 };
