d1c713af13a2fbc687dcf6112351d6e7bfb28d9d
gahyeon <gus_05@naver.com>
2025-01-20 18:02:54 +0900

    refactor: 로그인 관련 데이터 constant로 분리

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -1,39 +1,28 @@
 import KakaoBtn from '@components/common/button/kakaoBtn/KakaoBtn';
 import PageName from '@components/common/pageName/PageName';
+import LOGIN_INFOS from '@constants/loginInfos';
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import * as styles from './loginPage.css';
 
+type LoginType = 'my' | 'wish';
+
 const LoginPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const type = location.state?.type || 'my';
+
+  const type: LoginType = location.state?.type || 'my';
+
+  const { title, text, imageSrc, alt } = LOGIN_INFOS[type];
 
   return (
     <section className={styles.loginWrapper}>
-      <PageName
-        title={type === 'my' ? '마이페이지' : '위시리스트'}
-        onLeftClick={() => navigate(-1)}
-        isLikeBtn={false}
-      />
+      <PageName title={title} onLeftClick={() => navigate(-1)} isLikeBtn={false} />
       <div className={styles.contentWrapper}>
-        <h2 className={styles.textStyle}>
-          {type === 'my'
-            ? '절로가를 200% 즐기기 위해\n로그인 하러가기'
-            : '나만의 위시리스트를\n만들고 싶다면 로그인을 해주세요'}
-        </h2>
+        <h2 className={styles.textStyle}>{text}</h2>
 
-        <img
-          src={
-            type === 'my'
-              ? 'src/assets/images/img_yellow_light_smile.png'
-              : 'src/assets/images/img_pink_light_smile.png'
-          }
-          alt={type === 'my' ? '마이페이지 로그인' : '위시리스트 로그인'}
-          className={styles.imgStyle}
-          width={270}
-        />
+        <img src={imageSrc} alt={alt} className={styles.imgStyle} width={270} />
       </div>
       <KakaoBtn />
     </section>
