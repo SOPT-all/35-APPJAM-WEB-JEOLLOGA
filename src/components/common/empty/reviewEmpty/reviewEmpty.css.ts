5b30116f2c5dd6c56bac3274d44ede3d486dcc96
seong-hui <msh9980@naver.com>
2026-05-30 23:21:22 +0900

    fix: 리뷰 빈 상태 노출 및 디자인 통일

---
+++ src/components/common/empty/reviewEmpty/reviewEmpty.css.ts
@@ -0,0 +1,18 @@
+import theme from '@styles/theme.css';
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '0.6rem',
+  marginTop: '8rem',
+});
+
+export const textStyle = style({
+  ...theme.FONTS.h4Sb17,
+  color: theme.COLORS.black,
+  whiteSpace: 'pre-line',
+  textAlign: 'center',
+  marginBottom: '5rem',
+});
